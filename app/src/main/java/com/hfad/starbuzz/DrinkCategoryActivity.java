4a4bb9063c25d28c11498cd8191c8adb8e3b57b1
artytim <arty.tim@gmail.com>
2017-10-18 18:38:05 +0800

    Change Starbuzz to use a database •	Create the database o	The SQLiteOpenHelper manages your database o	Create database o	Create table •	Create the Drink table and populate it with drinks •	Change DrinkActivity to use the Starbuzz database o	Get a reference to the Starbuzz database o	Create a cursor to read drink data from the database o	Navigate to the drink record o	Display details of the drink in DrinkActivity •	Change DrinkCategoryActivity to use the Starbuzz database o	Create a cursor to read drink data from the database o	Replace the list view’s array adapter with a cursor adapter 	Use a simple cursor adapter to display cursor data in a list view; it takes columns from a cursor and maps them to text views or image view

--- app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -2,28 +2,49 @@ package com.hfad.starbuzz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends Activity {
+
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
-
-        // Create the array adapter
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
-                this,   // the current activity
-                android.R.layout.simple_list_item_1, // built-in layout resource
-                Drink.drinks);  // the array
         ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
-        listDrinks.setAdapter(listAdapter);
 
-        //Create the listener
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            cursor = db.query("DRINK",
+                    new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+
+            // Create the cursor adapter
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},          // Map the contents of the NAME column
+                    new int[]{android.R.id.text1}, // to the text in the ListView
+                    0);
+            listDrinks.setAdapter(listAdapter); // Set the adapter to the list view
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        // Create a listener to listen for clicks in the list view
         AdapterView.OnItemClickListener itemClickListener =
                 new AdapterView.OnItemClickListener() {
                     @Override
@@ -41,5 +62,13 @@ public class DrinkCategoryActivity extends Activity {
 
         //Assign the listener to the list view
         listDrinks.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    public void onDestroy(){
+        // Close the database and the cursor
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
